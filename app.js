32fcc442b765ce05bd56c7aa41fd9dea06ef3ade
andGustavo <ags9@aluno.ifal.edu.br>
2025-03-15 03:55:41 -0300

    Projeto Concluído.

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = ["Leticia", "Marina", "Zig", "Guilherme", "Fernando", "Marcos", "Ailton", "Pedro", "Roberto", "Matheus", "Tati", "Mauricio", "Auri", "Klud"];
+
+function adicionarAmigo(){
+    let NewAmigo = document.getElementById("amigo").value.trim(); 
+    if (NewAmigo == ''){
+        alert("Por favor, insira um nome.")
+    } else {
+        amigos.push(NewAmigo);
+        AtualizaLista();
+        document.getElementById("amigo").value = '';
+    } 
+}
+function AtualizaLista(){
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = '';
+    amigos.forEach(amigo => {
+        let itemLista = document.createElement("li");
+
+        itemLista.textContent = amigo;
+
+        listaAmigos.appendChild(itemLista);
+    });
+}
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert("Adicione amigos.")
+    } else {
+        let sort = Math.floor(Math.random() * amigos.length)
+        let amigoSort = amigos[sort]
+        document.getElementById("resultado").textContent = `O amigo sorteado foi: ${amigoSort}`;
+    }
+}
